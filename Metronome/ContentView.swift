1478b330f2bd190b1d30e55d36fa7f11d7a2cdf1
Josip Cavar <josipcavar04@gmail.com>
2024-10-11 08:55:31 +0200

    Initial project setup

---
+++ Metronome/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Metronome
+//
+//  Created by Josip Cavar on 13.09.2024..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Metronome/MetronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetronomeApp.swift
+//  Metronome
+//
+//  Created by Josip Cavar on 13.09.2024..
+//
+
+import SwiftUI
+
+@main
+struct MetronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
